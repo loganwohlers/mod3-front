4ffabecbe33c50f07c14a8a97e69a36c6fe71168
Logan Wohlers <lawohlers@gmail.com>
2019-04-18 09:23:19 -0700

    current final build rails end

--- rails2/app/controllers/api/v1/card_sets_controller.rb
+++ rails2/app/controllers/api/v1/card_sets_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::CardSetsController < ApplicationController
      def filter
           name=JSON.parse(request.raw_post)['name']
           set=CardSet.find_by(name: name)
-          @set_cards = set.cards.order(name: :asc).limit(500)
+          @set_cards = set.cards.order(name: :asc).limit(1000)
           render json: @set_cards
      end
   
